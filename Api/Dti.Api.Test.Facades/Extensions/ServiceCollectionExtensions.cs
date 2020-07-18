8b50119d714c5580da186cede1288f9af304ae7c
lfmundim <lucasfmundim@ufmg.br>
2020-07-18 11:02:40 -0300

    startup

---
+++ Api/Dti.Api.Test.Facades/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Dti.Api.Test.Models;
+using Dti.Api.Test.Models.UI;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Serilog;
+using Serilog.Exceptions;
+
+namespace Dti.Api.Test.Facades.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        private const string APPLICATION_KEY = "Application";
+        private const string SETTINGS_SECTION = "Settings";
+
+        /// <summary>
+        /// Registers project's specific services
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        public static void AddSingletons(this IServiceCollection services, IConfiguration configuration)
+        {
+            var settings = configuration.GetSection(SETTINGS_SECTION).Get<ApiSettings>();
+
+            // Dependency injection
+            services.AddSingleton(settings)
+                    .AddSingleton(settings.BlipBotSettings);
+
+            // SERILOG settings
+            services.AddSingleton<ILogger>(new LoggerConfiguration()
+                     .ReadFrom.Configuration(configuration)
+                     .Enrich.WithMachineName()
+                     .Enrich.WithProperty(APPLICATION_KEY, Constants.PROJECT_NAME)
+                     .Enrich.WithExceptionDetails()
+                     .CreateLogger());
+        }
+    }
+}
---
+++ Api/Dti.Api.Test/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+using Dti.Api.Test.Models;
+
+using Lime.Protocol;
+
+using Microsoft.AspNetCore.Http;
+
+using Newtonsoft.Json;
+
+using Serilog;
+
+namespace Dti.Api.Test.Middleware
+{
+    /// <summary>
+    /// Wraps all controller actions with a try-catch latch to avoid code repetition
+    /// </summary>
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+        public ErrorHandlingMiddleware(RequestDelegate next, ILogger logger)
+        #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Invoke Method, to validate requisition errors
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            // Thrown whenever a RestEase call returns with a non-success HttpStatusCode
+            if (exception is RestEase.ApiException apiException)
+            {
+                _logger.Error(apiException, "[{@user}] Error: {@exception}", context.Request.Headers[Constants.BLIP_USER_HEADER], exception.Message);
+                context.Response.StatusCode = (int)apiException.StatusCode;
+            }
+            else
+            {
+                _logger.Error(exception, "[{@user}] Error: {@exception}", context.Request.Headers[Constants.BLIP_USER_HEADER], exception.Message);
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            }
+            
+            string body = string.Empty;
+            using (var reader = new StreamReader(context.Request.Body))
+            {
+                body = await reader.ReadToEndAsync();
+            }
+
+            _logger.Error(exception, "[traceId:{@traceId}]{@user} Error. Headers: {@headers}. Query: {@query}. Path: {@path}. Body: {@body}",
+                          context.TraceIdentifier, context.Request.Headers[Constants.BLIP_USER_HEADER],
+                          context.Request.Headers, context.Request.Query, context.Request.Path, body);
+
+            context.Response.ContentType = MediaType.ApplicationJson;
+            await context.Response.WriteAsync(JsonConvert.SerializeObject($"{exception.Message}| traceId: {context.TraceIdentifier}"));
+        }
+    }
+}
